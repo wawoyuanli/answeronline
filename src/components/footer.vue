fb32d507e08338dbced2398000a4294a1b3d47c4
wawoyuanli <1583649818@qq.com>
2021-05-07 21:32:21 +0800

    update

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,137 +1,137 @@
 <template>
-	<div class="footer-box pt-5">
-		<div class="container mt-5 mb-5">
-			<div class="row">
-				<div class="col-md-3 text-light text-left">
-					<img
-						src="@/assets/images/yunlogo.png"
-						alt=""
-						width="100px"
-						height="100px"
-					/>
-					<p class="mt-3 mb-3">
-						American Consumer Opinion® is owned and operated by Decision
-						Analyst, Inc., a global marketing research and analytical consulting
-						firm headquartered in the Dallas-Fort Worth area in the United
-						States.
-					</p>
-					<p class="col-md-8">Headquarters: 604 Ave H E Arlington, TX 76011</p>
-					<p>Email: aparks@acop.com</p>
-				</div>
-				<div class="col-md-7 text-light text-left">
-					<p>MENU</p>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/faq" class="text-light text-decoration-none">FAQ</a>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/security" class="text-light text-decoration-none"
-							>Security</a
-						>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/consumerrights" class="text-light text-decoration-none"
-							>Consumer Rights</a
-						>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/terms" class="text-light text-decoration-none">Terms</a>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/privacyPolicy" class="text-light text-decoration-none"
-							>Privacy Policy</a
-						>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/contactUs" class="text-light text-decoration-none"
-							>Contact Us</a
-						>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/language" class="text-light text-decoration-none"
-							>Language</a
-						>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="#/affiliate" class="text-light text-decoration-none"
-							>Affiliate
-						</a>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a
-							href="#/personalInfoRequest"
-							class="text-light text-decoration-none"
-							>Personal Information Requests</a
-						>
-					</li>
-					<li style="display: flex;height:30px">
-						<img
-							src="@/assets/images/right-thick.png"
-							alt=""
-							width="40px"
-							height="40px"
-						/>
-						<a href="" class="text-light text-decoration-none"
-							>Do Not Sell My Personal Information</a
-						>
-					</li>
-				</div>
-				<div class="col-md-2 text-light">
-					<ul
-						style="
+  <div class="footer-box pt-5">
+    <div class="container mt-5 mb-5">
+      <div class="row">
+        <div class="col-md-3 text-light text-left">
+          <img
+            src="@/assets/images/yunlogo.png"
+            alt=""
+            width="100px"
+            height="100px"
+          />
+          <p class="mt-3 mb-3">
+            American Consumer Opinion® is owned and operated by Decision
+            Analyst, Inc., a global marketing research and analytical consulting
+            firm headquartered in the Dallas-Fort Worth area in the United
+            States.
+          </p>
+          <p class="col-md-8">Headquarters: 604 Ave H E Arlington, TX 76011</p>
+          <p>Email: aparks@acop.com</p>
+        </div>
+        <div class="col-md-7 text-light text-left">
+          <p>MENU</p>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/faq" class="text-light text-decoration-none">FAQ</a>
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/security" class="text-light text-decoration-none"
+              >Security</a
+            >
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/consumerrights" class="text-light text-decoration-none"
+              >Consumer Rights</a
+            >
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/terms" class="text-light text-decoration-none">Terms</a>
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/privacyPolicy" class="text-light text-decoration-none"
+              >Privacy Policy</a
+            >
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/contactUs" class="text-light text-decoration-none"
+              >Contact Us</a
+            >
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/language" class="text-light text-decoration-none"
+              >Language</a
+            >
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="#/affiliate" class="text-light text-decoration-none"
+              >Affiliate
+            </a>
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a
+              href="#/personalInfoRequest"
+              class="text-light text-decoration-none"
+              >Personal Information Requests</a
+            >
+          </li>
+          <li style="display: flex;height:30px">
+            <img
+              src="@/assets/images/right-thick.png"
+              alt=""
+              width="40px"
+              height="40px"
+            />
+            <a href="" class="text-light text-decoration-none"
+              >Do Not Sell My Personal Information</a
+            >
+          </li>
+        </div>
+        <div class="col-md-2 text-light">
+          <ul
+            style="
 							width: 220px;
 							list-style: none;
 							margin: 0px;
@@ -139,131 +139,164 @@
 							background: #292929;
 							border: 1px solid gray;
 						"
-					>
-						<a
-							href="http://www.facebook.com"
-							class="text-decoration-none border-bottom"
-							style="display: flex; justify-content: center"
-						>
-							<img
-								src="@/assets/images/facebook.png"
-								alt=""
-								width="40px"
-								height="40px"
-								class="mt-1 mb-1"
-							/>
-							<div style="width: 160px">
-								<div class="ml-3 text-left text-light">ACOP</div>
-								<div class="ml-3 text-left text-light">on Facebook</div>
-							</div>
-						</a>
-						<a
-							href="http://www.twitter.com"
-							class="text-decoration-none border-bottom"
-							style="display: flex; justify-content: center"
-						>
-							<img
-								src="@/assets/images/Twitter.png"
-								alt=""
-								width="40px"
-								height="40px"
-							/>
-							<div style="width: 160px" class="">
-								<div class="ml-3 text-left text-light">ACOP</div>
-								<div class="ml-3 text-left text-light">on Twitter</div>
-							</div>
-						</a>
-						<a
-							href="http://www.pinterest.com"
-							class="text-decoration-none border-bottom"
-							style="display: flex; justify-content: center"
-						>
-							<img
-								src="@/assets/images/pinterest.png"
-								alt=""
-								width="40px"
-								height="40px"
-								class="mt-1 mb-1"
-							/>
-							<div style="width: 160px">
-								<div class="ml-3 text-left text-light">ACOP</div>
-								<div class="ml-3 text-left text-light">on pinterest</div>
-							</div>
-						</a>
+          >
+            <a
+              href="http://www.facebook.com"
+              class="text-decoration-none border-bottom"
+              style="display: flex; justify-content: center"
+            >
+              <img
+                src="@/assets/images/facebook.png"
+                alt=""
+                width="40px"
+                height="40px"
+                class="mt-1 mb-1"
+              />
+              <div style="width: 160px">
+                <div class="ml-3 text-left text-light">ACOP</div>
+                <div class="ml-3 text-left text-light">on Facebook</div>
+              </div>
+            </a>
+            <a
+              href="http://www.twitter.com"
+              class="text-decoration-none border-bottom"
+              style="display: flex; justify-content: center"
+            >
+              <img
+                src="@/assets/images/Twitter.png"
+                alt=""
+                width="40px"
+                height="40px"
+              />
+              <div style="width: 160px" class="">
+                <div class="ml-3 text-left text-light">ACOP</div>
+                <div class="ml-3 text-left text-light">on Twitter</div>
+              </div>
+            </a>
+            <a
+              href="http://www.pinterest.com"
+              class="text-decoration-none border-bottom"
+              style="display: flex; justify-content: center"
+            >
+              <img
+                src="@/assets/images/pinterest.png"
+                alt=""
+                width="40px"
+                height="40px"
+                class="mt-1 mb-1"
+              />
+              <div style="width: 160px">
+                <div class="ml-3 text-left text-light">ACOP</div>
+                <div class="ml-3 text-left text-light">on pinterest</div>
+              </div>
+            </a>
 
-						<a
-							href="https://wwd.com/tag/instagram/"
-							class="text-decoration-none"
-							style="display: flex; justify-content: center"
-						>
-							<img
-								src="@/assets/images/Instagram.png"
-								alt=""
-								width="40px"
-								height="40px"
-								class="mt-1 mb-1"
-							/>
-							<div style="width: 160px">
-								<div class="ml-3 text-left text-light">ACOP</div>
-								<div class="ml-3 text-left text-light">on Instagram</div>
-							</div>
-						</a>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div class="container-fluid">
-			<div class="row pt-5 pb-5" style="background: #333333">
-				<div class="col-md-2"></div>
-				<div class="col-md-4">
-					<p class="text-light text-left">
-						© Copyright linkcloudsurvey, 2008 - 2021 All rights reserved.
-					</p>
-					<div class="text-light text-left">
-						<a href="" class="text-light text-decoration-none" id="terms">Terms of Use</a>
-						/
-						<a href="" class="text-light text-decoration-none"
-							>Privacy Policy</a
-						>
-					</div>
-				</div>
-				<div class="col-md-4 text-light text-right">
-					<div>img</div>
-					<div>aparks@acop.com @ACOPanel</div>
-				</div>
-				<div class="col-md-2"></div>
-			</div>
-		</div>
-	</div>
+            <a
+              href="https://wwd.com/tag/instagram/"
+              class="text-decoration-none"
+              style="display: flex; justify-content: center"
+            >
+              <img
+                src="@/assets/images/Instagram.png"
+                alt=""
+                width="40px"
+                height="40px"
+                class="mt-1 mb-1"
+              />
+              <div style="width: 160px">
+                <div class="ml-3 text-left text-light">ACOP</div>
+                <div class="ml-3 text-left text-light">on Instagram</div>
+              </div>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row pt-5 pb-5" style="background: #333333">
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+          <p class="text-light text-left">
+            © Copyright linkcloudsurvey, 2008 - 2021 All rights reserved.
+          </p>
+          <div class="text-light text-left">
+            <a href="" class="text-light text-decoration-none" id="terms"
+              >Terms of Use</a
+            >
+            /
+            <a href="" class="text-light text-decoration-none"
+              >Privacy Policy</a
+            >
+          </div>
+        </div>
+        <div class="col-md-4 text-light text-right">
+          <div style="display:flex;margin-left:400px" class="mb-4">
+            <a href="http://www.twitter.com">
+              <img
+                src="@/assets/images/twitter01.png"
+                alt=""
+                width="30px"
+                height="30px"
+                class="mr-2"
+              />
+            </a>
+            <img
+              src="@/assets/images/pinterest01.png"
+              alt=""
+              width="30px"
+              height="30px"
+              class="mr-2"
+            />
+            <img
+              src="@/assets/images/Instagram, logo.png"
+              alt=""
+              width="30px"
+              height="30px"
+              class="mr-2"
+            />
+            <img
+              src="@/assets/images/facebook01.png"
+              alt=""
+              width="30px"
+              height="30px"
+              class="mr-2"
+            />
+          </div>
+          <div>aparks@acop.com @ACOPanel</div>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import $ from 'jquery'
+import $ from "jquery";
 export default {
-	name: 'Footer',
-	mounted() {
-		window.onload = function () {
-			// function $(id) {
-			// 	return document.getElementById(id)
-			// }
-			//鼠标进
-			$('terms').onmouseover = function () {
-				debugger
-				// this.style.display="none";
-				// $('mobileList').style.display = 'block'
-				//给当钱的添加样式
-				this.style.color = 'red'
-			}
-			//鼠标出
-			// $('headerMobile').onmouseout = function () {
-			// 	$('mobileList').style.display = 'none'
-			// 	this.style.boxShadow = 'none'
-			// }
-		}
-	},
-}
+  name: "Footer",
+  mounted() {
+    window.onload = function() {
+      // function $(id) {
+      // 	return document.getElementById(id)
+      // }
+      //鼠标进
+      $("terms").onmouseover = function() {
+        debugger;
+        // this.style.display="none";
+        // $('mobileList').style.display = 'block'
+        //给当钱的添加样式
+        this.style.color = "red";
+      };
+      //鼠标出
+      // $('headerMobile').onmouseout = function () {
+      // 	$('mobileList').style.display = 'none'
+      // 	this.style.boxShadow = 'none'
+      // }
+    };
+  },
+};
 </script>
 <style scoped lang="less">
 .footer-box {
-	background-color: #3c5997;
+  background-color: #3c5997;
 }
 </style>
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,39 +1,48 @@
 <template>
-	<div class="container-fluid fixed-top">
-		<!-- style="background:#275894" -->
-		<!-- background: linear-gradient(#3d65ca, #54a5f6)-->
-		<div class="row shadow" style="background:#275894" >
-			<div class="col-md-4 text-left" style="border-right:1px solid #b8b8bc">
-				<a href="#/home">
-					<img
-						src="@/assets/images/yunlogo.png"
-						alt=""
-						width="82px"
-						height="83px"
-						class="ml-5"
-					/>
-				</a>
-				<div class="text-light pt-1 pb-1 ml-5">GLOBALTON CONSULTING INC</div>
-			</div>
-			<div class="col-md-6 text-right border-right">
-				<ul class="nav justify-content-end mt-5">
-					<li class="nav-item">
-						<a class="nav-link active text-light h6" href="#/home">HOME</a>
-					</li>	
-					<li class="nav-item">
-						<a class="nav-link text-light h6" href="#/signup">SIGN UP</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link text-light h6" href="#/login">LOGIN</a>
-					</li>
-				</ul>
-			</div>
-			<div class="col-md-2 mt-3">
-				<img src="@/assets/images/pinterest.png" alt="" class="mt-4 mr-3" />
-				<img src="@/assets/images/Instagram.png" alt="" class="mt-4 mr-3" />
-				<img src="@/assets/images/facebook.png" alt="" class="mt-4 mr-3" />
-				<img src="@/assets/images/Twitter.png" alt="" class="mt-4 mr-3" />
-			</div>
-		</div>		
-	</div>
+  <div class="container-fluid fixed-top">
+    <!-- style="background:#275894" -->
+    <!-- background: linear-gradient(#3d65ca, #54a5f6)-->
+    <div class="row shadow" style="background:#275894">
+      <div class="col-md-4 text-left" style="border-right:1px solid #b8b8bc">
+        <a href="#/home">
+          <img
+            src="@/assets/images/yunlogo.png"
+            alt=""
+            width="82px"
+            height="83px"
+            class="ml-5"
+          />
+        </a>
+        <div class="text-light pt-1 pb-1 ml-5">GLOBALTON CONSULTING INC</div>
+      </div>
+      <div class="col-md-6 text-right border-right">
+        <ul class="nav justify-content-end mt-5">
+          <li class="nav-item">
+            <a class="nav-link active text-light h6" href="#/home">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light h6" href="#/signup">SIGN UP</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light h6" href="#/login">LOGIN</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-2 mt-3">
+        <a href="http://www.pinterest.com">
+          <img src="@/assets/images/pinterest.png" alt="" class="mt-4 mr-3"
+        /></a>
+        <a href="https://wwd.com/tag/instagram">
+          <img src="@/assets/images/Instagram.png" alt="" class="mt-4 mr-3" />
+        </a>
+
+        <a href="http://www.facebook.com"
+          ><img src="@/assets/images/facebook.png" alt="" class="mt-4 mr-3"
+        /></a>
+        <a href="http://www.twitter.com">
+          <img src="@/assets/images/Twitter.png" alt="" class="mt-4 mr-3" />
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
